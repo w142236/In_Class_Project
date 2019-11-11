8006a3d7b99366d371f2c761eb0f0c7b9572dd5c
William Kett <william.kett-1@ou.edu>
2019-11-11 14:17:34 -0600

    Stuff

---
+++ Driver.java
@@ -0,0 +1,10 @@
+
+public class Driver {
+	public static void main(String[] args) {
+	Student student1 = new Student("Amanda", "", "113309008");
+	Student student2 = new Student("Karen", "", "737090989");
+	Student student3 = new Student("Aaron", "", "123555344");
+	Student student4 = new Student("Amanda", "", "123413212");
+	Student student5 = new Student("Kevin", "", "345622367");
+	}
+}
---
+++ Student.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Student {
+protected HashMap<String,String> studentHashMap = new HashMap<>();
+	public Student(String name, String nickname, String ID) {
+		// TODO Auto-generated constructor stub
+		
+	}
+}
